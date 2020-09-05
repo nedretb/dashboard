bce5fbd507629b5896d44551413888f01002eee5
nedretb <nedret.jr@gmail.com>
2020-09-05 13:35:51 +0200

    a

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
-let drop = document.getElementById("drop-menu");
+let drop_menu = document.getElementById("drop-menu");
+let drop = document.getElementById("drop");
+let arrow = document.getElementById("arrow");
 
 drop.addEventListener('click', event => {
-    drop.style.display = "none";
+    if(drop_menu.style.display == "none"){
+        drop_menu.style.display = "block";
+        arrow.className = "fas fa-angle-down";
+    }
+    else{
+        drop_menu.style.display = "none";
+        arrow.className = "fas fa-chevron-right";
+    }
 });
